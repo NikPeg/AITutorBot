7942fa1cea1cbeb37bbf92dfe8b845df3ea28058
NikPeg <peganov.nik@gmail.com>
2024-06-18 23:23:21 +0300

    send message to admin

--- tmain.py
+++ tmain.py
@@ -1,12 +1,26 @@
 import logging
 
+import config
 from aiogram import executor
+from aiogram import types
 
-import handlers
-from loader import dp
+from loader import bot, dp
+from utils import mess
 
 logging.basicConfig(level=logging.INFO)
 
 
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands([
+        types.BotCommand("start", "Запустить бота"),
+        types.BotCommand("help", "Помощь"),
+    ])
+
+
+async def on_startup(dispatcher):
+    await bot.send_message(config.ADMIN_ID, mess.BOT_STARTED.format(config.INSTANCE_NAME))
+    await set_default_commands(dispatcher)
+
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
